2325f5478c1428320d9a7bb2ce9785510ee6fa20
PixeL <officialpixelinc@gmail.com>
2024-01-26 21:48:44 -0600

    update polybar configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,3 +1,4 @@
+export POLYBAR_SHELL=/bin/bash
 killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
--- .config/polybar/tz_utc.sh
+++ .config/polybar/tz_utc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep_pid=0
+
 FORMAT_SHORT="%R %Z"
 FORMAT_LONG="%a %b %d, %Y %H:%M:%S %p %Z"
 FORMAT=$FORMAT_SHORT
@@ -14,6 +16,10 @@ update_format_type() {
   elif [ "$FORMAT" == "$FORMAT_LONG" ]; then
     FORMAT=$FORMAT_SHORT
   fi
+
+  if [ "$sleep_pid" -ne 0 ]; then
+    kill $sleep_pid >/dev/null 2>&1
+  fi
 }
 
 click() {
@@ -26,5 +32,6 @@ trap "click" USR1
 while true; do
   print_date
   sleep 1 &
+  sleep_pid=$!
   wait
 done
